567739eecf12d2fc6efa944a3e7830b841da70bb
Ian <nonzeronucleus@ianplumb.org>
2021-04-05 16:05:50 +0100

    Renames action text

--- src/features/covid/fetchCasesByAgeRange.js
+++ src/features/covid/fetchCasesByAgeRange.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getCasesByAgeRange } from './covidAPI';
 
 export const fetchCasesByAgeRange = createAsyncThunk(
-  'users/fetchCasesByArea',
+  'covid/fetchCasesByArea',
   async () => {
     const covidData = await getCasesByAgeRange();
     return {covidData};
